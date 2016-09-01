501c16f9eed2074c764e8bdcc8cb0bb316fbf631
Anmolgarg703 <anmolgarg703@gmail.com>
2016-09-01 08:06:34 +0530

    Adding Temperature sensors. Temperature and Pressure yet not working

--- app/src/main/java/com/example/anmol/hazewatch/Readings.java
+++ app/src/main/java/com/example/anmol/hazewatch/Readings.java
@@ -26,6 +26,7 @@ public class Readings extends Activity implements SensorEventListener{
     private TextView accelerometer;
     private TextView pressure;
     private TextView magnetometer;
+    private TextView temperature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +36,7 @@ public class Readings extends Activity implements SensorEventListener{
         accelerometer = (TextView) findViewById(R.id.accelerometer);
         magnetometer = (TextView) findViewById(R.id.magnetometer);
         pressure = (TextView) findViewById(R.id.pressure);
+        temperature = (TextView) findViewById(R.id.temperature);
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -61,9 +63,15 @@ public class Readings extends Activity implements SensorEventListener{
             magnetometer.setText("No Magnetic Sensors");
             Toast.makeText(this, "No Magnetic Sensors", Toast.LENGTH_SHORT).show();
         }
-
+        if(mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE) !=null){
+            Sensor temperature = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+            mSensorManager.registerListener(this, temperature, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        else{
+            temperature.setText("No Temperature Sensors");
+            Toast.makeText(this, "No Temperature Sensors", Toast.LENGTH_SHORT).show();
+        }
     }
-
 
     @Override
     public void onSensorChanged(SensorEvent event) {
@@ -72,6 +80,7 @@ public class Readings extends Activity implements SensorEventListener{
         float ax,ay,az;
         float mx,my,mz;
         float px,py,pz;
+        float tx,ty,tz;
 
         if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             ax = event.values[0];
@@ -89,6 +98,12 @@ public class Readings extends Activity implements SensorEventListener{
             my = event.values[1];
             mz = event.values[2];
             magnetometer.setText("MX = " + mx + "\nMY = " + my + "\nMZ = " + mz);
+        }
+        else if (sensor.getType() == Sensor.TYPE_AMBIENT_TEMPERATURE) {
+            tx = event.values[0];
+            ty = event.values[1];
+            tz = event.values[2];
+            temperature.setText("TX = " + tx + "\nTY = " + ty + "\nTZ = " + tz);
         }
 
     }
